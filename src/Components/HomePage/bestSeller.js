c22b9ac92c0c3077c24f27ed729464c504b03f17
Hadian Rahmat <hadian.rahmat@gits.id>
2019-07-10 12:35:26 +0700

    category

--- src/Components/HomePage/bestSeller.js
+++ src/Components/HomePage/bestSeller.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ItemsCarousel from 'react-items-carousel';
 import '../../Assets/bestSellers.css'
 import data from '../../data/dataBestseller'
@@ -12,7 +12,7 @@ export default class BestSeller extends Component {
     });
   }
 
-//   createproduct = n => range(n).map(i => <div key={i} style={{ height: 100, background: '#ebebe0' }}>{i}</div>);
+  //   createproduct = n => range(n).map(i => <div key={i} style={{ height: 100, background: '#ebebe0' }}>{i}</div>);
 
   changeActiveItem = (activeItemIndex) => this.setState({ activeItemIndex });
 
@@ -23,46 +23,48 @@ export default class BestSeller extends Component {
     } = this.state;
 
     return (
-        <div>
+      <div>
         <h1 className="title">Best Sellers</h1>
-      <ItemsCarousel
-        // Placeholder configurations
-        enablePlaceholder
-        numberOfPlaceholderItems={2}
-        placeholderItem={
-        <div className="container">{data.map(item =>{return(
-            <Link to={`/detail/${item.id}`}>
-                                        <div  className="item" id="items" productid={item.id}>
-                                            <img src={item.image_url} alt="gambar" />
-                                            <div>
-                                                <p className="barang">{item.productName}</p>
-                                                <p className="desc">{item.description}</p>
-                                                <p className="harga">{item.productPrice}</p>
-                                            </div>
-                                        </div>
-                                    </Link>
-        )})}</div>}
+        <ItemsCarousel
+          // Placeholder configurations
+          enablePlaceholder
+          numberOfPlaceholderItems={2}
+          placeholderItem={
+            <div className="container">{data.map(item => {
+              return (
+                <Link to={`/detail/${item.id}`}>
+                  <div className="item" id="items" productid={item.id}>
+                    <img src={item.image_url} alt="gambar" />
+                    <div>
+                      <p className="barang">{item.productName}</p>
+                      <p className="desc">{item.description}</p>
+                      <p className="harga">{item.productPrice}</p>
+                    </div>
+                  </div>
+                </Link>
+              )
+            })}</div>}
 
-        // Carousel configurations
-        numberOfCards={1}
-        gutter={3}
-        showSlither={true}
-        firstAndLastGutter={true}
-        freeScrolling={false}
+          // Carousel configurations
+          numberOfCards={1}
+          gutter={3}
+          showSlither={true}
+          firstAndLastGutter={true}
+          freeScrolling={false}
 
-        // Active item configurations
-        requestToChangeActive={this.changeActiveItem}
-        activeItemIndex={activeItemIndex}
-        activePosition={'center'}
+          // Active item configurations
+          requestToChangeActive={this.changeActiveItem}
+          activeItemIndex={activeItemIndex}
+          activePosition={'center'}
 
-        chevronWidth={15}
-        rightChevron={'>'}
-        leftChevron={'<'}
-        outsideChevron={false}
-      >
-        {product}
-      </ItemsCarousel>
+          chevronWidth={100}
+          rightChevron={'>'}
+          leftChevron={'<'}
+          outsideChevron={false}
+        >
+          {product}
+        </ItemsCarousel>
       </div>
-    );  
+    );
   }
 } 
